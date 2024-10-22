ab7dcc2edd0e679a70802646921b0e3136862b2d
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-10-22 13:45:05 -0300

    Add project files.

---
+++ sensor_project/Sensors/Temperature/TemperatureSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sensor_project.Sensors.Temperature
+{
+    public class TemperatureSensor
+    {
+        private Random random;
+
+        public TemperatureSensor()
+        {
+            random = new Random();
+        }
+
+        // Simulação de valores de temperatura entre -10 e 40 graus Celsius
+        public double GetSimulatedTemperature()
+        {
+            return Math.Round(random.NextDouble() * 50 - 10, 2); // entre -10°C e 40°C
+        }
+    }
+}
---
+++ sensor_project/UI/Views/MainDashboard.xaml.cs
@@ -0,0 +1,40 @@
+﻿using sensor_project.Sensors.Temperature;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sensor_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TemperatureSensor temperatureSensor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            temperatureSensor = new TemperatureSensor();
+
+            // Atualiza o label com a temperatura simulada
+            UpdateTemperatureLabel();
+        }
+        private void UpdateTemperatureLabel()
+        {
+            // Obtém a temperatura do sensor simulado
+            double temperature = temperatureSensor.GetTemperature();
+
+            // Atualiza o texto do Label com a temperatura obtida
+            temperatureLabel.Content = $"Temperatura: {temperature}°C";
+        }
+    }
+}
